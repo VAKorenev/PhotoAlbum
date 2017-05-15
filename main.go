ff2329312748a811130faf8e58aeb1756bfba1c4
QnX <korenev@gmail.com>
2017-05-15 20:56:35 +0300

    Новый старт
    Add поиск файлов
    Add конфиг файл

--- main.go
+++ main.go
@@ -2,11 +2,53 @@
 package main
 
 import (
+	"PhotoAlbum/ini-master"
 	"fmt"
 	"io"
-	"log"
 	"os"
 )
+
+type server struct {
+	ip   string
+	port string
+}
+
+func (s *server) load(f *ini.File) {
+	ip, err := f.Section("server").GetKey("ip")
+	if err != nil {
+		s.ip = "127.0.0.1"
+	}
+	s.ip = ip.String()
+	port, err := f.Section("server").GetKey("port")
+	if err != nil {
+		s.port = "8080"
+	}
+	s.port = port.String()
+}
+
+type data struct {
+	folder string
+}
+
+func (d *data) load(f *ini.File) {
+	folder, err := f.Section("data").GetKey("folder")
+	if err != nil {
+		d.folder = "."
+	}
+	d.folder = folder.String()
+}
+
+type html struct {
+	title string
+}
+
+func (h *html) load(f *ini.File) {
+	title, err := f.Section("html").GetKey("title")
+	if err != nil {
+		h.title = "Моя фотогаллерея"
+	}
+	h.title = title.String()
+}
 
 func readdir(dir string) {
 	dh, _ := os.Open(dir)
@@ -26,7 +68,16 @@ func readdir(dir string) {
 }
 
 func main() {
-
-	readdir(".")
-
+	c, err := ini.Load("PhotoAlbum.conf")
+	if err != nil {
+		panic(err)
+	}
+	s := new(server)
+	s.load(c)
+	d := new(data)
+	d.load(c)
+	h := new(html)
+	h.load(c)
+	readdir(d.folder)
+	fmt.Println(s.ip, s.port, d.folder, h.title)
 }
